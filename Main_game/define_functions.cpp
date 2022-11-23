6c8cdffa256f0c54173c99e5f59dce326f6b3ba4
Your Name Comes Here <IZDandarinov21@codingburgas.bg>
2022-11-23 22:16:32 +0200

    first commit/General layout

---
+++ Main_game/define_functions.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <vector>
+#include "main_functions.h"
+
+using namespace std;
+
+//declare functions
+
+void introduction()
+{
+
+    cout << "Hangman: The game" << endl;
+
+}
+
+void display_misses(int misses)
+{
+    if (misses == 0)
+    {
+        cout << "  +---+ " << endl;
+        cout << "  |   | " << endl;
+        cout << "      | " << endl;
+        cout << "      | " << endl;
+        cout << "      | " << endl;
+        cout << "      | " << endl;
+        cout << " ========= " << endl;
+    }
+    else if (misses == 1)
+    {
+        cout << "  +---+ " << endl;
+        cout << "  |   | " << endl;
+        cout << "  O   | " << endl;
+        cout << "      | " << endl;
+        cout << "      | " << endl;
+        cout << "      | " << endl;
+        cout << " ========= " << endl;
+    }
+    else if (misses == 2)
+    {
+        cout << "  +---+ " << endl;
+        cout << "  |   | " << endl;
+        cout << "  O   | " << endl;
+        cout << "  |   | " << endl;
+        cout << "      | " << endl;
+        cout << "      | " << endl;
+        cout << " ========= " << endl;
+    }
+    else if (misses == 3)
+    {
+        cout << "  +---+ " << endl;
+        cout << "  |   | " << endl;
+        cout << "  O   | " << endl;
+        cout << " /|   | " << endl;
+        cout << "      | " << endl;
+        cout << "      | " << endl;
+        cout << " ========= " << endl;
+    }
+    else if (misses == 4)
+    {
+        cout << "  +---+ " << endl;
+        cout << "  |   | " << endl;
+        cout << "  O   | " << endl;
+        cout << " /|\\  | " << endl;
+        cout << "      | " << endl;
+        cout << "      | " << endl;
+        cout << " ========= " << endl;
+    }
+    else if (misses == 5)
+    {
+        cout << "  +---+ " << endl;
+        cout << "  |   | " << endl;
+        cout << "  O   | " << endl;
+        cout << " /|\\  | " << endl;
+        cout << " /    | " << endl;
+        cout << "      | " << endl;
+        cout << " ========= " << endl;
+    }
+    else if (misses == 6)
+    {
+        cout << "  +---+ " << endl;
+        cout << "  |   | " << endl;
+        cout << "  O   | " << endl;
+        cout << " /|\\  | " << endl;
+        cout << " / \\  | " << endl;
+        cout << "      | " << endl;
+        cout << " ========= " << endl;
+    }
+
+}
+
+
+void display_status(vector<char> incorrect, string answer)
+{
+    cout << "Wrong answers:" << endl;
+
+    for (int i = 0; i < incorrect.size(); i++)
+    {
+        cout << incorrect[i] << " ";
+    }
+
+    cout << "\nCodeword:" << endl;
+
+    for (int i = 0; i < answer.size(); i++)
+    {
+        cout << answer[i] << " ";
+    }
+}
+
+void end_game(string answer, string codeword)
+{
+    if (answer == codeword)
+    {
+        cout << "Good job you saved the man from being hanged!" << endl;
+        cout << "Congratulations!" << endl;
+    }
+    else
+    {
+        cout << "Oh, NO the man was hanged!" << endl;
+    }
+}
---
+++ Main_game/main_functions.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//define functions
+
+void introduction();
+void display_misses(int misses);
+void display_status(std::vector<char> incorrect, string answer);
+void end_game(string answer, string codeword);
