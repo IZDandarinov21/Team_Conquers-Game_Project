be33f3d2efdfbe3a34bb0512e9e24f95f3e4d9d8
Атанас Апостолов <ATApostolov21@codingburgas.bg>
2022-11-24 21:11:00 +0200

    The problem with the menu is fixed

--- Main_game/Menu.cpp
+++ Main_game/Menu.cpp
@@ -7,32 +7,29 @@ using namespace std;
 void Menu()
 {
     int choice;
-    ifstream iFile;
-    ofstream oFile;
+    
     do
     {
-        cout << "0. Play\n1. Settings\n2. creits\n3. exit\n\n";
+        cout << "1. Play\n2. Settings\n3. credits\n4. exit\n\n";
         cin >> choice;
         switch (choice)
         {
-        case 0:            
-            iFile.open("Main_game.cpp");
-            if (!iFile.is_open()) {
-                cout << "Error";
-            }
-        case 1:
-            cout << "Your in the settings\n";
+        case 1:            
+            cout << "You play the game\n";
             break;
         case 2:
-            cout << "by appleninja360YT \n";
+            cout << "Your in the settings\n";
             break;
         case 3:
+            cout << "by ATApostolov and IVAdandarinov \n";
+            break;
+        case 4:
             cout << "Ok Bye\n";
             break;
         }
 
 
-    } while (choice != 3);
+    } while (choice != 1);
 
 }
 
